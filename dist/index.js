ff44f11562e534aaf1927ab747f5f3f1a364f264
Mitesh Shah <mitesh@mokko.io>
2019-06-23 20:21:11 -0500

    Update built version.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
-import { Box, Button, Card, CardContent, CardHeader, Section, Container, Columns, Column, Tile } from 'bloomer';
+import { Box, Button, Card, CardContent, CardHeader, Section, Container, Columns, Column, Tile, Title } from 'bloomer';
 
-export { Box, Button, Card, CardContent, CardHeader, Container, Section, Columns, Column, Tile };
+export { Box, Button, Card, CardContent, CardHeader, Container, Section, Columns, Column, Tile, Title };
