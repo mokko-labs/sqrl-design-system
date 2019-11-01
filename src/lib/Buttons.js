578b09c0593c0055164e85a24b90140812370c90
Mitesh Shah <mitesh@mokko.io>
2019-10-31 23:29:03 -0500

    Primary, secondary, tertiary.

--- src/lib/Buttons.js
+++ src/lib/Buttons.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Button } from 'bloomer'
 
-export default ({ className, ...rest }) => (
+export const PrimaryButton = ({ className, ...rest }) => (
   <Button className={`primary ${className}`} {...rest} />
+)
+
+export const SecondaryButton = ({ className, ...rest }) => (
+  <Button className={`secondary ${className}`} {...rest} />
+)
+
+export const TertiaryButton = ({ className, ...rest }) => (
+  <Button className={`tertiary ${className}`} {...rest} />
 )
--- src/screens/Buttons.js
+++ src/screens/Buttons.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { CustomButton as Button, Section, Container } from '../lib'
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+  Section,
+  Container
+} from '../lib'
 
 export default () => (
   <Section>
@@ -10,16 +16,41 @@ export default () => (
 
       <h2>Primary Button</h2>
       <div className="sqrl-util-spread">
-        <Button>Get Started</Button>
-        <Button className="hover">Get Started</Button>
-        <Button className="active">Get Started</Button>
-        <Button className="disabled">Get Started</Button>
+        <PrimaryButton>Get Started</PrimaryButton>
+        <PrimaryButton className="hover">Get Started</PrimaryButton>
+        <PrimaryButton className="active">Get Started</PrimaryButton>
+        <PrimaryButton disabled>Get Started</PrimaryButton>
 
-        <Button className="focus">Get Started</Button>
-        <Button className="hover focus">Get Started</Button>
-        <Button className="active focus">Get Started</Button>
+        <PrimaryButton className="focus">Get Started</PrimaryButton>
+        <PrimaryButton className="hover focus">Get Started</PrimaryButton>
+        <PrimaryButton className="active focus">Get Started</PrimaryButton>
       </div>
 
+      <hr />
+      <h2>Secondary Button</h2>
+      <div className="sqrl-util-spread">
+        <SecondaryButton>Get Started</SecondaryButton>
+        <SecondaryButton className="hover">Get Started</SecondaryButton>
+        <SecondaryButton className="active">Get Started</SecondaryButton>
+        <SecondaryButton disabled>Get Started</SecondaryButton>
+
+        <SecondaryButton className="focus">Get Started</SecondaryButton>
+        <SecondaryButton className="hover focus">Get Started</SecondaryButton>
+        <SecondaryButton className="active focus">Get Started</SecondaryButton>
+      </div>
+
+      <hr />
+      <h2>Tertiary Button</h2>
+      <div className="sqrl-util-spread">
+        <TertiaryButton>Get Started</TertiaryButton>
+        <TertiaryButton className="hover">Get Started</TertiaryButton>
+        <TertiaryButton className="active">Get Started</TertiaryButton>
+        <TertiaryButton disabled>Get Started</TertiaryButton>
+
+        <TertiaryButton className="focus">Get Started</TertiaryButton>
+        <TertiaryButton className="hover focus">Get Started</TertiaryButton>
+        <TertiaryButton className="active focus">Get Started</TertiaryButton>
+      </div>
       <br />
     </Container>
   </Section>
