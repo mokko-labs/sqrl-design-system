7ad8eb7d872967ce6de6bf076ce650f06a59e44a
Mitesh Shah <mitesh@mokko.io>
2020-01-02 20:25:35 -0600

    Fix warning.

--- src/screens/Demo.js
+++ src/screens/Demo.js
@@ -99,12 +99,6 @@ let states = {
   WY: 'Wyoming'
 }
 
-const encode = data => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
-
 const RenderCheckbox = ({ label, ...rest }) => {
   return (
     <Control {...rest}>
