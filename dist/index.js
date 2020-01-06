8518db965373f24a9e5a4f3f1a05006d7f9e12cf
Mitesh Shah <mitesh@mokko.io>
2020-01-05 19:09:36 -0600

    Expand pill.

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,6 @@ export { default as CustomSelect } from './components/CustomSelect';
 export { default as Switch } from './components/Switch';
 export { default as Loader } from './components/Loader';
 export { default as StyledLine } from './components/StyledLine';
+export { default as Pill } from './components/Pill';
 export { default as InPlaceNotification } from './components/InPlaceNotification';
 export * from './components/Buttons';
